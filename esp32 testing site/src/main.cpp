c2d7523b4f158e3a1ad91adb32f2d5bb0eba7182
Stephen Macabulos <myteseeker18@gmail.com>
2025-11-15 07:05:46 +0800

    Add initial project structure and configuration files for ESP32 development

---
+++ esp32 testing site/src/main.cpp
@@ -0,0 +1,25 @@
+#define VRX_PIN  39 // ESP32 pin GPIO39 (ADC3) connected to VRX pin
+#define VRY_PIN  36 // ESP32 pin GPIO36 (ADC0) connected to VRY pin
+
+int valueX = 0; // to store the X-axis value
+int valueY = 0; // to store the Y-axis value
+
+void setup() {
+  Serial.begin(9600);
+
+  // Set the ADC attenuation to 11 dB (up to ~3.3V input)
+  analogSetAttenuation(ADC_11db);
+}
+
+void loop() {
+  // read X and Y analog values
+  valueX = analogRead(VRX_PIN);
+  valueY = analogRead(VRY_PIN);
+
+  // print data to Serial Monitor on Arduino IDE
+  Serial.print("x = ");
+  Serial.print(valueX);
+  Serial.print(", y = ");
+  Serial.println(valueY);
+  delay(200);
+}
