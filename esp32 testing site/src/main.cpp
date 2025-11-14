ef71ade661e7b8ade806ec90cda3fe3eec438c61
Stephen Macabulos <myteseeker18@gmail.com>
2025-11-15 07:37:13 +0800

    Refactor main.cpp to improve command handling and update platformio.ini configuration

--- esp32 testing site/src/main.cpp
+++ esp32 testing site/src/main.cpp
@@ -1,8 +1,23 @@
+
+#include <Arduino.h>
+
 #define VRX_PIN  39 // ESP32 pin GPIO39 (ADC3) connected to VRX pin
 #define VRY_PIN  36 // ESP32 pin GPIO36 (ADC0) connected to VRY pin
 
-int valueX = 0; // to store the X-axis value
-int valueY = 0; // to store the Y-axis value
+#define LEFT_THRESHOLD  1000
+#define RIGHT_THRESHOLD 3000
+#define UP_THRESHOLD    1000
+#define DOWN_THRESHOLD  3000
+
+#define COMMAND_NO     0x00
+#define COMMAND_LEFT   0x01
+#define COMMAND_RIGHT  0x02
+#define COMMAND_UP     0x04
+#define COMMAND_DOWN   0x08
+
+int valueX = 0 ; // to store the X-axis value
+int valueY = 0 ; // to store the Y-axis value
+int command = COMMAND_NO;
 
 void setup() {
   Serial.begin(9600);
@@ -16,10 +31,42 @@ void loop() {
   valueX = analogRead(VRX_PIN);
   valueY = analogRead(VRY_PIN);
 
-  // print data to Serial Monitor on Arduino IDE
-  Serial.print("x = ");
-  Serial.print(valueX);
-  Serial.print(", y = ");
-  Serial.println(valueY);
-  delay(200);
+  // converts the analog value to commands
+  // reset commands
+  command = COMMAND_NO;
+
+  // check left/right commands
+  if (valueX < LEFT_THRESHOLD)
+    command = command | COMMAND_LEFT;
+  else if (valueX > RIGHT_THRESHOLD)
+    command = command | COMMAND_RIGHT;
+
+  // check up/down commands
+  if (valueY < UP_THRESHOLD)
+    command = command | COMMAND_UP;
+  else if (valueY > DOWN_THRESHOLD)
+    command = command | COMMAND_DOWN;
+
+  // NOTE: AT A TIME, THERE MAY BE NO COMMAND, ONE COMMAND OR TWO COMMANDS
+
+  // print command to serial and process command
+  if (command & COMMAND_LEFT) {
+    Serial.println("COMMAND LEFT");
+    // TODO: add your task here
+  }
+
+  if (command & COMMAND_RIGHT) {
+    Serial.println("COMMAND RIGHT");
+    // TODO: add your task here
+  }
+
+  if (command & COMMAND_UP) {
+    Serial.println("COMMAND UP");
+    // TODO: add your task here
+  }
+
+  if (command & COMMAND_DOWN) {
+    Serial.println("COMMAND DOWN");
+    // TODO: add your task here
+  }
 }
